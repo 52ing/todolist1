16ba560621288532e89eaf35cf0348b763cbea31
Seulki Lee <seulkilee@SeulkiLeeui-MacBookPro.local>
2023-01-28 19:47:10 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+import Item from './components/Item';
+import TodoList from './components/TodoList';
+const App = () => {
+    return (
+        <>
+            <TodoList/>
+        </>
+    );
+}
+export default App;
